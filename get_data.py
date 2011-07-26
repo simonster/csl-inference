8236a64a79ee9a7ef5ab9d2ef21f887cfe2b2340
Simon Kornblith <simon@simonster.com>
2011-07-26 14:33:39 -0400

    Add AGPLv3 license

--- get_data.py
+++ get_data.py
@@ -1,4 +1,25 @@
 #!/opt/local/bin/python2.7
+"""
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2011 Simon Kornblith
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+"""
+
 import os, glob, re, string
 from xml.etree.ElementTree import ElementTree, tostring
 
